1cb7bbaf8346a0400997c351c86ca61466bcf3d8
Ahmed Z. <azaied7550@gmail.com>
2025-03-05 16:42:58 -0500

    Ramadan Project

---
+++ src/main/java/org/mma/training/java/rest/SpringBootRestApiApplication.java
@@ -0,0 +1,21 @@
+package org.mma.training.java.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiApplication.class, args);
+	}
+
+	@GetMapping("/welcome")
+	public String greeting(){
+		return "Ramadan Mubarak";
+	}
+
+}
